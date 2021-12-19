0b5a2819ae7681e51d731dbe3a14a7eb68efe1cc
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-12-19 17:05:13 +0100

    Update 03.py

--- 03.py
+++ 03.py
@@ -20,6 +20,24 @@ def get_new_candidates(bit_position, most_common_bit, candidates):
             new_candidates.append(number)
     return new_candidates
 
+def get_ratings(gas):
+    candidates = [number for number in inputlines]
+    for bit_position in range(num_len):
+        most_common, least_common = find_most_common_bits(bit_position, candidates)
+        if most_common[1] == least_common[1]:
+            if gas == 'o2':
+                most_common_bit = '1'
+            elif gas == 'co2':
+                most_common_bit = '0'
+        else:
+            if gas == 'o2':
+                most_common_bit = most_common[0]
+            elif gas == 'co2':
+                most_common_bit = least_common[0]
+        candidates = get_new_candidates(bit_position, most_common_bit, candidates)
+        if len(candidates) == 1:
+            return candidates[0]
+
 #Part 1
 
 gamma_rate = ''
@@ -33,30 +51,4 @@ print(int(gamma_rate, 2) * int(epsilon_rate, 2))
 
 #Part 2
 
-o2_candidates = [number for number in inputlines]
-
-for bit_position in range(num_len):
-    most_common, least_common = find_most_common_bits(bit_position, o2_candidates)
-    if most_common[1] == least_common[1]:
-        most_common_bit = '1'
-    else:
-        most_common_bit = most_common[0]
-    o2_candidates = get_new_candidates(bit_position, most_common_bit, o2_candidates)
-    if len(o2_candidates) == 1:
-        o2_rating = o2_candidates[0]
-        break
-
-co2_candidates = [number for number in inputlines]
-
-for bit_position in range(num_len):
-    most_common, least_common = find_most_common_bits(bit_position, co2_candidates)
-    if most_common[1] == least_common[1]:
-        most_common_bit = '0'
-    else:
-        most_common_bit = least_common[0]
-    co2_candidates = get_new_candidates(bit_position, most_common_bit, co2_candidates)
-    if len(co2_candidates) == 1:
-        co2_rating = co2_candidates[0]
-        break
-
-print(int(o2_rating, 2) * int(co2_rating, 2))
+print(int(get_ratings('o2'), 2) * int(get_ratings('co2'), 2))
