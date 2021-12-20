93d0ac45be46b8ffa4d0786464afd081b88c7880
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-12-20 09:16:04 +0100

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,27 +1,23 @@
 #Day 01
 
-with open('./01.txt') as myinput:
-    inputlines = myinput.readlines()
+with open('./01.txt') as my_input:
+    input_lines = my_input.readlines()
 
-measurements = list(map(int, inputlines))
+measurements = list(map(int, input_lines))
 
-#Part 1
-
-counter = 0
-for current_measurement, next_measurement in zip(measurements, measurements[1:]):
-    if next_measurement > current_measurement:
-        counter += 1
-
-print(counter)
-
-#Part 2
-
-counter = -1
-current_sum = 0
+counter_1, counter_2, current_sum = 0, -1, 0
 for first_measurement, second_measurement, third_measurement in zip(measurements, measurements[1:], measurements[2:]):
+    if second_measurement > first_measurement:
+        counter_1 += 1
     previous_sum = current_sum
     current_sum = first_measurement + second_measurement + third_measurement
     if current_sum > previous_sum:
-        counter += 1
+        counter_2 += 1
 
-print(counter)
+#Part 1
+
+print(counter_1)
+
+#Part 2
+
+print(counter_2)
