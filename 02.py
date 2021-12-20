6fc0ea025963caadf1bfd44a42f3c8e4dcf5f1aa
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-12-20 09:19:17 +0100

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,25 +1,9 @@
 #Day 02
 
-with open('./02.txt') as myinput:
-    inputlines = myinput.readlines()
+with open('./02.txt') as my_input:
+    input_lines = my_input.readlines()
 
-commands = [command.split() for command in inputlines]
-commands = [(direction, int(value)) for direction, value in commands]
-
-#Part 1
-
-x, y = 0, 0
-for direction, value in commands:
-    if direction == 'forward':
-        x += value
-    elif direction == 'up':
-        y -= value
-    elif direction == 'down':
-        y += value
-
-print(x*y)
-
-#Part 2
+commands = [(direction, int(value)) for direction, value in [command.split() for command in input_lines]]
 
 x, y, aim = 0, 0, 0
 for direction, value in commands:
@@ -30,5 +14,11 @@ for direction, value in commands:
         aim -= value
     elif direction == 'down':
         aim += value
+
+#Part 1
+
+print(x*aim)
+
+#Part 2
 
 print(x*y)
